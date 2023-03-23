4c804308447962b6e333d1e5f5e8a786b5a6fd86
Michael Duberstein <mduberstein@gmail.com>
2023-03-22 20:38:00 -0400

    After Clip New Clip 10 Constructs demo

--- cdk-stuff/first-cdk/lib/first-cdk-stack.ts
+++ cdk-stuff/first-cdk/lib/first-cdk-stack.ts
@@ -1,14 +1,39 @@
 import * as cdk from 'aws-cdk-lib';
-import { aws_s3 as s3, CfnOutput, Duration } from 'aws-cdk-lib';
+import { aws_s3 as s3, CfnOutput, CfnParameter, Duration } from 'aws-cdk-lib';
+import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
+class L3Bucket extends Construct {
+  constructor(scope: Construct, id: string, expiration: number ) {
+    super(scope, id);
+    new Bucket(scope, 'L3Bucket', {
+      lifecycleRules: [
+        {
+          expiration: Duration.days(expiration)
+        }
+      ]
+    })
+  }
+}
 export class FirstCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-    const myBucket = new s3.Bucket(this, 'someBucket', {
+    // 3 buckets
+
+
+    new CfnBucket(this, 'MyL1Bucket', {
+      lifecycleConfiguration: {
+        rules: [{
+          expirationInDays: 1,
+          status: 'Enabled'
+        }]
+      }
+    });
+
+    new Bucket(this, 'MyL2Bucket', {
       lifecycleRules: [
         {
           expiration: Duration.days(2)
@@ -16,9 +41,22 @@ export class FirstCdkStack extends cdk.Stack {
       ]
     });
 
-    new CfnOutput(this, 'mybucket', {
-      value: myBucket.bucketName
-    })
+    new L3Bucket(this, 'MyL3Bucket', 3);
+
+    // before 2023-03-23_BEGIN
+    // const myBucket = new s3.Bucket(this, 'someBucket', {
+    //   lifecycleRules: [
+    //     {
+    //       expiration: Duration.days(2)
+    //     }
+    //   ]
+    // });
+
+    // new CfnOutput(this, 'mybucket', {
+    //   value: myBucket.bucketName
+    // })
+    // before 2023-03-23_END
+
     // example resource
     // const queue = new sqs.Queue(this, 'FirstCdkQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
